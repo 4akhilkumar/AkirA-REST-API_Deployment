80a056be0a05eaef7621d4616d0fd3feaecbc7ca
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-12-25 12:07:57 +0530

    Deployment Commit V1.0

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn akira_rest_api.wsgi --log-file -
--- akira_rest_api/settings.py
+++ akira_rest_api/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w*2iejhzpmm5#n7gx_gt&qpsuyv)3y@h)qkr=)%mt%#^0aua#p'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://akira-rest-api.herokuapp.com/', 'akira-rest-api.herokuapp.com', '*']
 
 
 # Application definition
@@ -126,3 +128,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE= True
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_AGE = 14400
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
